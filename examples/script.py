b37a95556774228c25f1914017937a42fafd1576
Iulia Chiriac <iulia.chiriac@yahoo.com>
2022-12-19 12:24:07 +0200

    Add script.py

---
+++ examples/script.py
@@ -0,0 +1,27 @@
+"""
+This is a docstring for module script.py
+"""
+
+print("Hello world")
+
+# implicit line joining
+text = ("If we have a long logical line, we can split it into physical lines "
+        "by using the backslash at the end of the line so that Python "
+        "understands that the next physical line should be considered as part "
+        "of the current logical line.")
+my_shopping_list = ['apples', 'bananas', 'oranges', 'pears', 'peaches',
+                    'cherries']
+
+# explicit line joining - using \
+text_2 = "If we have a long logical line, we can split it into physical lines"\
+         " by using the backslash at the end of the line so that Python"\
+         " understands that the next physical line should be considered as "\
+         "part of the current logical line."
+
+x = 0
+
+if x > 0:
+    print("yes")
+    print("still under if")
+
+print("outside if")
