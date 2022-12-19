81cd0cd723b7504d23a912bdc2fe01d4c3e1b828
Iulia Chiriac <iulia.chiriac@yahoo.com>
2022-12-19 16:38:55 +0200

    More strings examples

---
+++ examples/strings.py
@@ -0,0 +1,18 @@
+
+multiline_str = """
+abs(x) (absolute value)
+int(x) (conversion to integer)
+float(x) (conversion to floating point)
+"""
+print(multiline_str)
+
+print("Escape double quote: \"; Escape backslash: \\")
+
+my_str = "absolute value"
+print("Substring in string:", "subs" in my_str)
+
+price = 4.34253
+print(f"Price: {price:.2f} $")
+
+greeting = "hello!"
+print(f"{greeting.title()[:-1]} from Maria. {len} is a function.")
