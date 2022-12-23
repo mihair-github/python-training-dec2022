6eb11b8d135ff5415ab375d1a7f89fb1624f55e0
Iulia Chiriac <iulia.chiriac@yahoo.com>
2022-12-23 11:32:16 +0200

    Add property

--- examples/decorators.py
+++ examples/decorators.py
@@ -13,6 +13,23 @@ def make_pretty(func):  # func is a local variable for make_pretty
     return inner  # inner will replace func
 
 
+def doubler(func):  # func is a local variable for make_pretty
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return result * 2
+
+    return inner  # inner will replace func
+
+
+@make_pretty
+@doubler
+def get_sum(a, b, c):
+    return a + b + c
+# get_sum = make_pretty(doubler(get_sum))
+# get_sum = make_pretty(doubler(params)(get_sum))
+
+
 def print_exec_time(func):
     def inner(*args, **kvargs):
         t1 = time.time()
@@ -21,6 +38,7 @@ def print_exec_time(func):
         print(f'Function run for: {t2 - t1} s')
         return inner_result
     return inner
+
 
 @make_pretty
 def ordinary():
@@ -55,3 +73,6 @@ print("Decremented value:", decremented_val, end="\n\n")
 
 print(f"Decrement function's name is <{decrement.__name__}> and does "
       f"<{decrement.__doc__}>.")
+
+
+print("Sum is", get_sum(1, 2, 3))
--- examples/oop.py
+++ examples/oop.py
@@ -1,34 +1,81 @@
+from datetime import date
+
+
 class Person:
     count = 0  # class variable
+    MIN_DATE_OF_BIRTH = date(1940, 1, 1)
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, date_of_birth: date):
         self.name = name  # instance variable
-        print("inside __init__", self, id(self))
-        self.increment_count()
+        self._date_of_birth = date_of_birth
+        print("Current instance (inside init)", self, id(self), )
+        print("Current class (inside init):", self.__class__, id(self.__class__))
+        self._increment_count()
+
+    @property
+    def date_of_birth(self):
+        # print("getter called!")
+        return self._date_of_birth
+
+    @date_of_birth.setter
+    def date_of_birth(self, value):
+        # print(f"setter called with {value}!")
+        if value < self.MIN_DATE_OF_BIRTH:
+            raise ValueError("Invalid date of birth")
+        self._date_of_birth = value
+
+    @date_of_birth.deleter
+    def date_of_birth(self):
+        # print("deleter called!")
+        del self._date_of_birth
+
+    @property
+    def age(self):
+        diff = date.today() - self.date_of_birth  # timedelta object
+        return int(diff.days / 365.25)
 
     def greet(self, greeting):  # instance method
         print(f"{self.name} says <{greeting.capitalize()}!>")
 
     @classmethod
-    def increment_count(cls):  # class method
+    def _increment_count(cls):  # class method
+        print("Current class (inside increment_count):", cls, id(cls))
         cls.count += 1
 
     @staticmethod
     def get_info():  # static method
         print("This is a static method!")
 
+    def __lt__(self, other):
+        return self.date_of_birth > other.date_of_birth
+
+    def __str__(self):
+        return f"Person {self.name} ({self.date_of_birth})"
+
 
 if __name__ == '__main__':
-    p1 = Person("Ana")
-    print(p1, id(p1), p1.name)
-    p2 = Person("Matei")
-    print(p2, id(p2), p2.name)
+    p1 = Person("Ana", date(1999, 12, 3))
+    # print("Current class (outside class):", Person, id(Person))
+    # print(f"Current instance (outside class): {p1} {id(p1)} {p1.name}")
+    # p2 = Person("Matei", date(1992, 4, 13))
+    # # print(p2, id(p2), p2.name)
+    #
+    # print(p1.count, p2.count, Person.count, p1.count is p2.count)
+    # print(p1.name is p2.name)
+    #
+    # p1.greet("hi")
+    # p2.greet("good morning")
+    #
+    # Person.get_info()
+    # p1.get_info()
+    #
+    # print(p1 < p2)
 
-    print(p1.count, p2.count, Person.count, p1.count is p2.count)
-    print(p1.name is p2.name)
+    print(p1.date_of_birth)  # getter for name
+    p1.date_of_birth = date(1989, 12, 3)  # setter for name
+    print(p1.date_of_birth)  # getter for name
 
-    p1.greet("hi")
-    p2.greet("good morning")
+    print(p1.age)
 
-    Person.get_info()
-    p1.get_info()
+    # del p1.date_of_birth  # deleter for name
+    # print(p1.date_of_birth)  # getter for name
